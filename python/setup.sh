38d18e111ad4372ad6bce2ac17620ccb457cc22f
Vegard Antun <vegarant@student.matnat.uio.no>
2017-10-24 12:11:45 +0200

    python/setup.sh: Checks python version

--- python/setup.sh
+++ python/setup.sh
@@ -7,5 +7,16 @@ swig -c++ -python hadamardKernel.i
 
 echo "Running python setup.py build_ext --inplace"
 
-python setup.py build_ext --inplace
+PYTHON_VERSION_STR=`python -V 2> /dev/stdout` 
+SUBSTRING=$(echo $PYTHON_VERSION_STR | cut -d' ' -f 2)
+VERSION=$(echo $SUBSTRING | cut -d'.' -f 1)
+
+if [ "$VERSION" -eq "2" ]
+then
+    python3 setup.py build_ext --inplace
+else
+    python setup.py build_ext --inplace
+fi
+
+
 
